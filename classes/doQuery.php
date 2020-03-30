08990c1247b8999806c1ce2e7f89ed7a7bcbf4d5
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-03-30 18:04:34 +0200

    do query select and insert to admin class only

--- classes/doQuery.php
+++ classes/doQuery.php
@@ -6,5 +6,5 @@
         public function selectAll($conn);
         // public function delete($conn , $tableName , $query);
         // public function update($conn , $tableName , $query);
-        // public function insert($conn , $tableName , $query);
+         public function insert($conn , User $user);
     }

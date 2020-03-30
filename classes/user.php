0256a316c475f795d4bc88e04f15dad47bef6fdd
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-03-30 16:46:41 +0200

    m

--- classes/user.php
+++ classes/user.php
@@ -2,7 +2,6 @@
 
     abstract class User{
             
-<<<<<<< HEAD
           public $firstName;
           public $lastName;
           public $pass;
@@ -16,21 +15,7 @@
       //           $this->email = $email;
 
       //     }
-=======
-          public $name;
-          public $pass;
-          public $email;
-           
-          public function __contruct($name , $pass , $email){
-                // $super();
-                $this->name = $name;
-                $this->pass = $pass;
-                $this->email = $email;
-
-          }
->>>>>>> 8e7b9b8ff7837be0c1644bd1300d6b8189517e59
-          
-          
+    
     }
 
 

54eb59ebbc4bb68bcae10a93bbf1e0bda4fc73b4
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-04-18 21:30:05 +0200

    rename function is_admin to getAllRegister
    i help us to know which function we want to check if this user have permition to access or not

--- app/Classes/Middleware.php
+++ app/Classes/Middleware.php
@@ -9,13 +9,13 @@ use App\Classes\Authorization;
 class Middleware implements Authentication{
     private $List = array("normal","ownershop");
 
-    public function is_admin($function,$type){
+    public function getAllRegister($type){
         if(in_array($type,$this->List)){
             return '/error';
         }
 
         $access = new Authorization();
-        return  $access->is_admin($function,$type)."<br>";
+        return  $access->getAllRegister($type)."<br>";
 
     }
 
